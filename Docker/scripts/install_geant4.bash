dad6288c6c466c748889a91dfe2101a32ba82215
Faruk D <f.diblen@esciencecenter.nl>
2022-04-23 16:43:26 +0200

    add custom Docker image with Geant4 and ROOT6

---
+++ Docker/scripts/install_geant4.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+pkgname=geant4
+pkgver=10.07.p03
+pkg_url=https://cern.ch/geant4-data/releases/${pkgname}.${pkgver}.tar.gz
+srcdir=$(pwd)/${pkgname}/src/${pkgver}
+insdir=$(pwd)/${pkgname}/${pkgver}
+
+[ -d ${srcdir} ] || mkdir -p ${srcdir}
+
+wget \
+    -P ${srcdir} \
+    -c ${pkg_url}
+
+cd ${srcdir}
+tar xvf geant4*.tar.gz
+
+[ -d ${srcdir}/build ] || mkdir ${srcdir}/build
+cd ${srcdir}/build
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${insdir} \
+    -DGEANT4_INSTALL_DATA=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DGEANT4_BUILD_MULTITHREADED=ON \
+    -DGEANT4_USE_GDML=ON \
+    -DGEANT4_USE_G3TOG4=OFF \
+    -DGEANT4_USE_QT=OFF \
+    -DGEANT4_USE_XM=OFF \
+    -DGEANT4_USE_OPENGL_X11=OFF \
+    -DGEANT4_USE_INVENTOR=OFF \
+    -DGEANT4_USE_RAYTRACER_X11=OFF \
+    -DGEANT4_USE_SYSTEM_CLHEP=OFF \
+    -DGEANT4_USE_SYSTEM_EXPAT=OFF \
+    -DGEANT4_USE_SYSTEM_ZLIB=OFF \
+    ../${pkgname}.${pkgver}
+
+G4VERBOSE=1 make -j4
+make install && rm -rf ${srcdir}
---
+++ Docker/scripts/install_root6.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pkgname=root
+pkgver=6.24.00
+pkg_url=https://root.cern/download/root_v${pkgver}.source.tar.gz
+srcdir=$(pwd)/${pkgname}/src/${pkgver}
+insdir=$(pwd)/${pkgname}/${pkgver}
+
+[ -d ${srcdir} ] || mkdir -p ${srcdir}
+
+wget \
+    -P ${srcdir} \
+    -c ${pkg_url}
+
+cd ${srcdir}
+tar xvf root*source.tar.gz
+
+[ -d ${srcdir}/build ] || mkdir ${srcdir}/build
+cd ${srcdir}/build
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${insdir} \
+    -Dgnuinstall=ON \
+    -Droofit=OFF \
+    -Dpython3=ON -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+    ../${pkgname}-${pkgver}
+
+make -j4
+make install && rm -rf ${srcdir}
+
+# export ROOTSYS=${insdir}
+# export PATH=$PATH:$ROOTSYS/bin
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib/root
