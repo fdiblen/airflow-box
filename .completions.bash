df936346ea2552a9fbc6e9b2980ca52e9234c049
Faruk D <f.diblen@esciencecenter.nl>
2022-04-24 11:54:56 +0000

    add .completions.bash

---
+++ .completions.bash
@@ -0,0 +1,13 @@
+complete -W "
+    --completions
+    --clean
+    --download
+    --generate
+    --pip
+    --setuppy
+    --shell
+    --start
+    --stop
+    --test
+    --verbose
+  " ./airflow_control.sh
