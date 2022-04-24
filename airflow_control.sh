17881fec7c47b76bc4c6d40ee8f60f35f488d881
Faruk D <f.diblen@esciencecenter.nl>
2022-04-24 12:07:33 +0000

    sort functions

--- airflow_control.sh
+++ airflow_control.sh
@@ -2,9 +2,72 @@
 
 # https://airflow.apache.org/docs/apache-airflow/stable/start/docker.html#docker-compose-yaml
 
+activate_python() {
+  echo -e "\nRunning activate_python()"
+  . ./venv/bin/activate
+  which pip
+}
+
+clean() {
+  echo -e "\nRunning clean()"
+  docker-compose down --remove-orphans --volumes --rmi all
+  docker stop $(docker ps -a -q) && docker rm -f $(docker ps -a -q)
+  docker rmi -f $(docker images -q)
+  docker network prune --force
+  docker volume prune --force
+  rm -rf logs plugins venv
+  rm -rf dags/__pycache__ dags/dynamic_dags/__pycache__
+}
+
 download() {
   echo -e "\nRunning download()"
   curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.2.5/docker-compose.yaml'
+}
+
+generate_dags() {
+  echo -e "\nRunning generate_dags()"
+  setup_python
+  . ./venv/bin/activate
+  python dags/dynamic_dags/generator.py
+}
+
+get_shell() {
+  echo -e "\nRunning get_shell()"
+  docker run --rm -ti --name airflow_tmp \
+    --entrypoint bash airflow_airflow-worker:latest
+}
+
+help() {
+  printf "\nUsage:\n"
+  printf "$0
+    [--completions]
+    [--clean|-c]
+    [--download|-d]
+    [--generate|-g]
+    [[--pip|-p] '<package1><package2>']
+    [--setuppy|--py]
+    [--shell]
+    [--start]
+    [--stop]
+    [--test|-t]
+    [--verbose|-v]\n"
+  exit 1
+}
+
+install_airflow_providers() {
+  echo -e "\nRunning install_airflow_providers()"
+  # https://airflow.apache.org/docs/apache-airflow/stable/concepts/operators.html
+  # https://airflow.apache.org/docs/apache-airflow-providers/packages-ref.html
+  pip install apache-airflow-providers-docker
+}
+
+install_pip_package() {
+  echo -e "\nRunning install_pip_package()"
+  # echo "\$@:" "$@"
+  # echo "\${extra_pip_packages}:" "${extra_pip_packages}"
+  echo "Installing pip packages:" "${extra_pip_packages}"
+  . ./venv/bin/activate
+  pip install "${extra_pip_packages}"
 }
 
 prepare() {
@@ -13,11 +76,39 @@ prepare() {
   docker-compose pull
 }
 
-update_compose() {
-  echo -e "\nRunning update_compose()"
-  sudo cp /usr/bin/docker-compose /usr/bin/docker-compose_old
-  sudo curl -L "https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64" -o /usr/bin/docker-compose
-  sudo chmod +x /usr/bin/docker-compose
+read_env_file() {
+  echo -e "\nRunning read_env_file()"
+  if [ -f .env ]; then
+      export $(grep -v '^#' .env | xargs)
+  fi
+}
+
+setup_python() {
+  echo -e "\nRunning setup_python()"
+  python3 -m venv venv
+  . ./venv/bin/activate
+  pip install --upgrade pip setuptools wheel
+  pip install jinja2 pyyaml
+}
+
+set_cli_completions() {
+  echo -e "\nRunning set_cli_completions()"
+  echo 'complete -W "
+    --completions
+    --clean
+    --download
+    --generate
+    --pip
+    --setuppy
+    --shell
+    --start
+    --stop
+    --test
+    --verbose
+  "' $0 > .completions.bash
+  echo -e "Run:\n
+    . ./.completions.bash
+  "
 }
 
 start() {
@@ -53,104 +144,11 @@ test() {
       "${ENDPOINT_URL}/api/v1/pools"
 }
 
-clean() {
-  echo -e "\nRunning clean()"
-  docker-compose down --remove-orphans --volumes --rmi all
-  docker stop $(docker ps -a -q) && docker rm -f $(docker ps -a -q)
-  docker rmi -f $(docker images -q)
-  docker network prune --force
-  docker volume prune --force
-  rm -rf logs plugins venv
-  rm -rf dags/__pycache__ dags/dynamic_dags/__pycache__
-}
-
-setup_python() {
-  echo -e "\nRunning setup_python()"
-  python3 -m venv venv
-  . ./venv/bin/activate
-  pip install --upgrade pip setuptools wheel
-  pip install jinja2 pyyaml
-}
-
-activate_python() {
-  echo -e "\nRunning activate_python()"
-  . ./venv/bin/activate
-  which pip
-}
-
-install_airflow_providers() {
-  echo -e "\nRunning install_airflow_providers()"
-  # https://airflow.apache.org/docs/apache-airflow/stable/concepts/operators.html
-  # https://airflow.apache.org/docs/apache-airflow-providers/packages-ref.html
-  pip install apache-airflow-providers-docker
-}
-
-install_pip_package() {
-  echo -e "\nRunning install_pip_package()"
-  # echo "\$@:" "$@"
-  # echo "\${extra_pip_packages}:" "${extra_pip_packages}"
-  echo "Installing pip packages:" "${extra_pip_packages}"
-  . ./venv/bin/activate
-  pip install "${extra_pip_packages}"
-}
-
-generate_dags() {
-  echo -e "\nRunning generate_dags()"
-  setup_python
-  . ./venv/bin/activate
-  python dags/dynamic_dags/generator.py
-}
-
-get_shell() {
-  echo -e "\nRunning get_shell()"
-  docker run --rm -ti --name airflow_tmp \
-    --entrypoint bash airflow_airflow-worker:latest
-}
-
-read_env_file() {
-  echo -e "\nRunning read_env_file()"
-  # read local .env file and set environment variables
-  if [ -f .env ]; then
-      export $(grep -v '^#' .env | xargs)
-  fi
-}
-
-set_cli_completions() {
-  echo -e "\nRunning set_cli_completions()"
-  echo 'complete -W "
-    --completions
-    --clean
-    --download
-    --generate
-    --pip
-    --setuppy
-    --shell
-    --start
-    --stop
-    --test
-    --verbose
-  "' $0 > .completions.bash
-  echo -e "Run:\n
-    . ./.completions.bash
-  "
-}
-
-
-help() {
-  printf "\nUsage:\n"
-  printf "$0
-    [--completions]
-    [--clean|-c]
-    [--download|-d]
-    [--generate|-g]
-    [[--pip|-p] '<package1><package2>']
-    [--setuppy|--py]
-    [--shell]
-    [--start]
-    [--stop]
-    [--test|-t]
-    [--verbose|-v]\n"
-  exit 1
+update_compose() {
+  echo -e "\nRunning update_compose()"
+  sudo cp /usr/bin/docker-compose /usr/bin/docker-compose_old
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64" -o /usr/bin/docker-compose
+  sudo chmod +x /usr/bin/docker-compose
 }
 
 main() {
