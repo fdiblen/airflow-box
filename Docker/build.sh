9fb2e11b4df941ffb0496a518f72bd8bd4597ffa
Faruk D <f.diblen@esciencecenter.nl>
2022-04-24 11:54:34 +0000

    add archlinux based Docker image

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t ptsim .
+docker build --no-cache --rm -t ptsim:arch-20220410 -t ptsim:arch-latest --file Dockerfile-arch .
+docker build --no-cache --rm -t ptsim:ubuntu-22.04 -t ptsim:ubuntu-latest --file Dockerfile-ubuntu .
